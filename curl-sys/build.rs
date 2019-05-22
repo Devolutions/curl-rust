54bebc91e3860474f97a47c1ce232f691ef6cc20
Marc-André Moreau <marcandre.moreau@gmail.com>
2019-05-22 16:54:07 -0400

    curl-sys: use conan-rs wrapper (supports non-conan builds)

--- curl-sys/build.rs
+++ curl-sys/build.rs
@@ -2,38 +2,30 @@ extern crate pkg_config;
 #[cfg(target_env = "msvc")]
 extern crate vcpkg;
 extern crate cc;
+extern crate conan;
 
 use std::env;
 use std::fs;
 use std::path::{PathBuf, Path};
 use std::process::Command;
 
-use std::fs::File;
-use std::io::prelude::*;
-
-fn conan_build() {
-    //let target = env::var("TARGET").unwrap();
-    let target_os = env::var("CARGO_CFG_TARGET_OS").unwrap();
-    let target_arch = env::var("CARGO_CFG_TARGET_ARCH").unwrap();
-    //let is_windows = if target_os == "windows" { true } else { false };
-
-    Command::new("conan")
-        .arg("install")
-        .arg("-pr")
-        .arg(format!("{}-{}", target_os, target_arch))
-        .arg("conanfile.txt")
-        .output()
-        .expect("failed to execute process");
-
-    let mut file = File::open("conanbuildinfo.cargo").expect("Error opening File");
-    let mut contents = String::new();
-    file.read_to_string(&mut contents).expect("Unable to read to string");
-    println!("{}", contents);
-}
+use conan::*;
 
 fn main() {
-    if let Ok(_) = env::var("CARGO_FEATURE_CONAN_BUILD") {
-        conan_build();
+    let target_os = env::var("CARGO_CFG_TARGET_OS").unwrap();
+    let target_arch = env::var("CARGO_CFG_TARGET_ARCH").unwrap();
+    let conan_profile = format!("{}-{}", target_os, target_arch);
+
+    let command = InstallCommandBuilder::new()
+        .with_profile(&conan_profile)
+        .build_type(BuildType::Release)
+        .build_policy(BuildPolicy::Missing)
+        .recipe_path(Path::new("conanfile.txt"))
+        .build();
+
+    if let Some(build_info) = command.generate() {
+        println!("using conan build info");
+        build_info.cargo_emit();
         return;
     }
 
